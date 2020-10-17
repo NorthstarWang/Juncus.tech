c1682e07e9c0e21c0902919af59f9f224a2d7263
NorthstarWang <61356439+NorthstarWang@users.noreply.github.com>
2020-10-17 19:57:06 +0800

    Switching done

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask,render_template
+import string
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template("index.html")
+
+
+@app.route('/Calculator/<string:subject>/<string:file>')
+def calculator(subject, file):
+    path = "calculator/" + subject + "/" + file
+    return render_template(path)
+
+
+@app.errorhandler(404)
+@app.errorhandler(500)
+def not_found(e):
+    return render_template("error/error.html")
 
 
 if __name__ == '__main__':
