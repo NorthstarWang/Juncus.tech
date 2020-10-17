26bf52b18a4340411247ed7da338ff7a80c86a17
NorthstarWang <61356439+NorthstarWang@users.noreply.github.com>
2020-10-17 22:57:41 +0800

    Basic calculation of earthquake intensity done

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import string
-
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+import math
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
 
 @app.route('/')
@@ -16,6 +14,19 @@ def index():
 def calculator(subject, file):
     path = "calculator/" + subject + "/" + file
     return render_template(path)
+
+
+@app.route('/Api/MHF4U/Earthquake/Intensity', methods=['POST'])
+def intensityCalculation1():
+    M1 = float(request.form['magnitude1'])
+    M2 = float(request.form['magnitude2'])
+    decimal = int(request.form['decimal'])
+    if 0 <= M1 <= 10 and 0 <= M2 <= 10:
+        difference = M1-M2
+        text = 'Earthquake 1 is ' + str(round(math.pow(10, difference), decimal)) + ' times as intense as earthquake 2.'
+        return text
+    else:
+        return str('Please type in correct value range from 0 to 10!')
 
 
 @app.errorhandler(404)
